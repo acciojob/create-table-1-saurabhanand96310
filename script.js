cd11cbe6b7ef6e2e176699c96c88a28cece5ad65
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-08-03 19:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function insert_Row() {
 	let newRow=table.insertRow(0);
 	let cell1=newRow.insertCell(0);
 	let cell2=newRow.insertCell(1);
-	cell1.innerText = "new cell1";
-cell2.innerText = "new cell2";
+	cell1.innerText = "New Cell1";
+cell2.innerText = "New Cell2";
 	
 }
